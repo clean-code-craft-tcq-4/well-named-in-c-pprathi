e5ad23fc0bfe2740a5fdcf888d552cd8d039c035
Prathibha Ponnuswamy <pprathi.01@gmail.com>
2022-06-14 23:50:53 +0530

    Delete NumberToPairValidation.h

--- NumberToPairValidation.h
+++
@@ -1,4 +0,0 @@
-
-void 		ValidateNumberToPair	(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor);
-ColorPair 	GetColorFromPairNumber	(int pairNumber);
-void 		ColorPairToString		(const ColorPair* colorPair, char* buffer);
